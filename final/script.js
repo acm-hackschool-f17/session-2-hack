0f3ba4ab47933b6e990f5681b6015231277db654
shannonphu <shannonphu@sbcglobal.net>
2017-10-25 01:33:32 -0700

    upload starter and final code for modal demo for Hack Session 2

---
+++ final/script.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	$("table tr").on("click", function(event){
+		let row = $(event.currentTarget);
+		let fruit = row.find(".fruit").first().text();
+		$(".popup .title").text(fruit);
+		$(".popup").removeClass("inactive");
+		$(".popup").addClass("active");
+		$(".container").css("opacity", "0.5");
+	});
+
+	$("button").on("click", function(event) {
+		$(".popup").addClass("inactive");
+		$(".popup").removeClass("active");
+		$(".container").css("opacity", "1");
+	});
+})
